5ea335bfb7ef09a2f22b4fb89b1aeedc098b0a10
AUGUSTINE-PC\AUGUSTINE <rmoayinla@yahoo.com>
2018-01-24 07:03:08 +0100

    partial file for template footers

---
+++ public/partials/footer.php
@@ -0,0 +1,10 @@
+		<!-- jQuery library -->
+		<script src="<?php echo base_url()?>public/assets/jquery/dist/jquery.min.js"></script>
+
+		<!-- Popper JS -->
+		<script src="<?php echo base_url()?>node_modules/popper.js/dist/umd/popper.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="<?php echo base_url();?>public/assets/bootstrap/dist/js/bootstrap.min.js"></script>
+	</body>
+</html>
