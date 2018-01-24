83441cf4d0417dc3c1adb3a2c0405b180ba0433f
AUGUSTINE-PC\AUGUSTINE <rmoayinla@yahoo.com>
2018-01-24 09:46:40 +0100

    Footer partial for the bottom contents

--- public/partials/footer.php
+++ public/partials/footer.php
@@ -1,3 +1,9 @@
+		<footer id="main-footer" class="container-fluid">
+			<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?>
+			</p>
+		</footer>
+	<!-- #wrapper div opened in partials/header.php -->
+	</div>
 		<!-- jQuery library -->
 		<script src="<?php echo base_url()?>public/assets/jquery/dist/jquery.min.js"></script>
 
