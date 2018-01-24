ee1c152be8ebaa59019f14c0fed1ab6c19f7fd2d
AUGUSTINE-PC\AUGUSTINE <rmoayinla@yahoo.com>
2018-01-24 02:57:38 +0100

    A static template, just for sampling will later have to be deleted

---
+++ public/about.php
@@ -0,0 +1,14 @@
+<?php
+?>
+<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="utf-8">
+			<title>About page</title>
+		</head>
+		<body>
+			<div>
+				<h2> About page </h2>
+				<p> sample page loading here  </p>
+		</body>
+	</html>
