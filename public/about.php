ceef50f2d064835a36e4d3e30457302a1fe20972
AUGUSTINE-PC\AUGUSTINE <rmoayinla@yahoo.com>
2018-01-25 14:04:11 +0100

    now using a universal template just as home.php

--- public/about.php
+++ public/about.php
@@ -1,14 +1,31 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<!DOCTYPE html>
-	<html lang="en">
-		<head>
-			<meta charset="utf-8">
-			<title>About page</title>
-		</head>
-		<body>
-			<div>
-				<h2> About page </h2>
-				<p> sample page loading here  </p>
-		</body>
-	</html>
+	<!--include and import header partial -->
+	<?php include VIEWPATH.'partials/header.php'; ?>
+
+		<header class="jumbotron jumbotron-fluid">
+			<div class="container-fluid">
+				<h1>About us!</h1>
+			</div>
+		</header>
+		<main id="content" class='row'>
+			
+			<div id="body" class="p-5">
+				<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+
+				<p>If you would like to edit this page you'll find it located at:</p>
+				<code>application/views/welcome_message.php</code>
+
+				<p>The corresponding controller for this page is found at:</p>
+				<code>application/controllers/Welcome.php</code>
+
+				<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+				
+			</div>
+
+			
+		</main>
+
+	<!-- include and import the site footer partial -->
+	<?php include VIEWPATH.'partials/footer.php'; ?>
