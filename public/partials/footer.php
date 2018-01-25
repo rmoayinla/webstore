8b862cdef017e40613b2f093bc43e18bb9be3c28
AUGUSTINE-PC\AUGUSTINE <rmoayinla@yahoo.com>
2018-01-25 14:03:15 +0100

    styled the footer with some bs classes

--- public/partials/footer.php
+++ public/partials/footer.php
@@ -1,4 +1,4 @@
-		<footer id="main-footer" class="container-fluid">
+		<footer id="main-footer" class="container-fluid bg-dark p-5 text-white">
 			<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?>
 			</p>
 		</footer>
