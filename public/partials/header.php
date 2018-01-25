2a0915628d7694557819f190cdb3670f6a59d03d
AUGUSTINE-PC\AUGUSTINE <rmoayinla@yahoo.com>
2018-01-25 14:01:44 +0100

    trying out sessions

--- public/partials/header.php
+++ public/partials/header.php
@@ -10,7 +10,7 @@
 	</head>
 
 	<body>
-		<div id="wrapper">
+		<div id="wrapper" class="site-wrapper">
 			<nav class="navbar navbar-expand-lg navbar-light bg-light" id="primary-menu" role="navigation">
 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#" 
 						aria-controls="" aria-expanded="false" aria-label="Toggle navigation"
@@ -20,10 +20,17 @@
 
 					<a class="navbar-brand" href="#">Webstore </a>
 					<div class="collapse navbar-collapse" id="">
-						<ul class="navbar-nav mx-auto">
+						<ul class="navbar-nav mx-lg-auto mx-sm-0">
 							<li class="nav-item"><a href="#" class="nav-link">Customers </a></li>
 							<li class="nav-item"><a href="#" class="nav-link">Suppliers </a></li>
 							<li class="nav-item"><a href="#" class="nav-link">Orders </a></li>
+
+						</ul>
+						<ul class="navbar-nav navbar-right">
+							<?php if(empty($session)): ?>
+								<li class="nav-item"><a href="#" class="nav-link"> Sign up </a></li>
+								<li class="nav-item"><a href="#" class="nav-link"> Sign in </a></li>
+							<?php endif; ?>
 						</ul>
 					</div>	
 			</nav>
