60ea40dc2a92ef1e65a29ec36e484df690d9ead7
AUGUSTINE-PC\AUGUSTINE <rmoayinla@yahoo.com>
2018-02-02 21:52:45 +0100

    customized the 404 error page to blend with my theme

--- public/errors/html/error_404.php
+++ public/errors/html/error_404.php
@@ -1,64 +1,23 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>404 Page Not Found</title>
-<style type="text/css">
+?>
+	<!--include and import header partial -->
+	<?php include VIEWPATH.'partials/header.php'; ?>
 
-::selection { background-color: #E13300; color: white; }
-::-moz-selection { background-color: #E13300; color: white; }
+		<header class="jumbotron jumbotron-fluid">
+			<div class="container-fluid">
+				<h1>404 Error page!</h1>
+			</div>
+		</header>
+		<main id="content" class='row'>
+			
+			<div id="body" class="p-5">
+				<p> The page you are trying to load does not exist </p>
+				<p> <?php if(!empty($content)) echo $content;?></p>
+			</div>
 
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
+			
+		</main>
 
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
-
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
-
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
-
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	box-shadow: 0 0 8px #D0D0D0;
-}
-
-p {
-	margin: 12px 15px 12px 15px;
-}
-</style>
-</head>
-<body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
-	</div>
-</body>
-</html>
+	<!-- include and import the site footer partial -->
+	<?php include VIEWPATH.'partials/footer.php'; ?>
