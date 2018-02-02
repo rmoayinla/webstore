da503dec28499c995f8c8bf28c73d93af7425da7
AUGUSTINE-PC\AUGUSTINE <rmoayinla@yahoo.com>
2018-02-02 22:38:46 +0100

    add some custom variable to the error 404 template

--- public/errors/html/error_404.php
+++ public/errors/html/error_404.php
@@ -6,14 +6,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 		<header class="jumbotron jumbotron-fluid">
 			<div class="container-fluid">
-				<h1>404 Error page!</h1>
+				<?php if(!empty($heading)): echo sprintf('<h1>%s</h1>',$heading); ?>
+				<?php else : ?>4<h1>404 Error page!</h1>
+				<?php endif; ?>
+				
 			</div>
 		</header>
 		<main id="content" class='row'>
 			
 			<div id="body" class="p-5">
 				<p> The page you are trying to load does not exist </p>
-				<p> <?php if(!empty($content)) echo $content;?></p>
+				<p> <?php if(!empty($message)) echo $message;?></p>
+				
 			</div>
 
 			
