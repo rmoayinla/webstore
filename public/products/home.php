590a693b5818f7b0179c431a68286592a1a9d401
AUGUSTINE-PC\AUGUSTINE <rmoayinla@yahoo.com>
2018-02-09 17:36:33 +0100

    A new template file for showing the base page for products

---
+++ public/products/home.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+	<!--include and import header partial -->
+	<?php include VIEWPATH.'partials/header.php'; ?>
+
+		<header class="jumbotron jumbotron-fluid">
+			<div class="container-fluid">
+				<h1>Products</h1>
+			</div>
+		</header>
+		<main id="content" class='container-fluid'>
+			
+			
+
+			
+			
+
+			
+		</main>
+
+	<!-- include and import the site footer partial -->
+	<?php include VIEWPATH.'partials/footer.php'; ?>
+
+		
