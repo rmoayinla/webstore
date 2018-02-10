1039fe697acaf4eb85b69791b7535385fe5e89d2
AUGUSTINE-PC\AUGUSTINE <rmoayinla@yahoo.com>
2018-02-10 01:12:54 +0100

    added a new route to add new products to the db

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-
+$route['products/([a-z]+)/(new|create)']['GET'] = 'product/create/$1';
 $route['products'] = 'product';
 
 $route['default_controller'] = 'welcome';
