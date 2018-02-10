2a4f586c478ecfa75612e1b6463815f172931149
AUGUSTINE-PC\AUGUSTINE <rmoayinla@yahoo.com>
2018-02-10 01:13:06 +0100

    commented some of my functions

--- application/helpers/WS_url_helper.php
+++ application/helpers/WS_url_helper.php
@@ -16,16 +16,28 @@ function ws_full_url($product){
 	if(!empty($product["slug"])) return base_url($product["slug"]);
 }
 
-function ws_archive_url($product){
+/**
+ * 
+ * Url helper function that generate archive links dynamically from the db for products
+ *
+ *@since: 0.1
+ *@return: string 	html formatted link 
+ *@param: 	$product 		(array)		array containing product info 
+			$attributes 	(array) 	array of attributes for the generated html a tag 
+ *
+ */
+function ws_archive_url($product, $attributes = array()){
 	$ci = $GLOBALS["ci"];
-
+	//bail early if we dont have a product type id //
 	if(empty($product["product_type_id"])) return base_url();
+
 	$type_id = $product["product_type_id"];
 	$sql = 'SELECT * FROM ws_products_type WHERE ID = ?';
 	$product_type = $ci->db->query($sql, $type_id);
 	$result = $product_type->result_array();
 	if(!empty($result)){
-		return base_url($result[0]["slug"]);
+		$text =sprintf('All %ss', ucwords($result[0]["name"]) );
+		return anchor(base_url($result[0]["slug"]), $text, $attributes);
 	}
 }
 
