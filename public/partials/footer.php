7f0d9e5b3c2938ee2cf37fcc2fbdadf9f6f34170
AUGUSTINE-PC\AUGUSTINE <rmoayinla@yahoo.com>
2018-02-12 20:11:15 +0100

    included tinymce.js and added a top margin to the footer wrapper

--- public/partials/footer.php
+++ public/partials/footer.php
@@ -1,4 +1,4 @@
-		<footer id="main-footer" class="container-fluid bg-dark p-5 text-white">
+		<footer id="main-footer" class="container-fluid bg-dark p-5 text-white mt-5">
 			<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?>
 			</p>
 		</footer>
@@ -12,5 +12,10 @@
 
 		<!-- Latest compiled JavaScript -->
 		<script src="<?php echo base_url();?>public/assets/bootstrap/dist/js/bootstrap.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="<?php echo base_url();?>node_modules/tinymce/tinymce.min.js"></script>
+
+		<script src="<?php echo base_url();?>public/js/script.js"></script>
 	</body>
 </html>
