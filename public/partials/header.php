be0575d6fcfb5d7f1178161f664f1642ff3e4745
AUGUSTINE-PC\AUGUSTINE <rmoayinla@yahoo.com>
2018-02-12 20:11:31 +0100

    just a simple link href tag

--- public/partials/header.php
+++ public/partials/header.php
@@ -11,7 +11,7 @@
 		<link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url();?>apple-touch-icon.png">
 		<link rel="icon" type="image/png" sizes="32x32" href="<?php echo base_url();?>favicon-32x32.png">
 		<link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url();?>favicon-16x16.png">
-		<?php echo link_tag("site.webmanifest", "manifest");?><link rel="" href="<?php echo base_url();?>">
+		<?php echo link_tag("site.webmanifest", "manifest");?>
 		<link rel="mask-icon" href="<?php echo base_url();?>safari-pinned-tab.svg" color="#5bbad5">
 		<meta name="msapplication-TileColor" content="#f1f7f7">
 		<meta name="msapplication-TileImage" content="<?php echo base_url();?>mstile-144x144.png">
