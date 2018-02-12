897e87ada898da3dc714cd5d56a4d3b2e0be0ec6
AUGUSTINE-PC\AUGUSTINE <rmoayinla@yahoo.com>
2018-02-12 20:13:58 +0100

    The products home welcome page, queries the db and shows some products

--- public/products/home.php
+++ public/products/home.php
@@ -10,8 +10,37 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			</div>
 		</header>
 		<main id="content" class='container-fluid'>
-			
-			
+			<div class="row">
+				<?php if(!empty($products)): ?>
+					<?php foreach($products as $product): ?>
+						<div class="col-md-4">
+
+							<div class="card">
+								<img class="card-img-top" src="..." alt="" style="height:180px;">
+								<div class="card-body">
+									<h3 class="card-title"><?php echo html_escape($product["title"]); ?></h3>
+									<h6 class="card-subtitle mb-2"><?php echo $product["created_date"]; ?></h6>
+									<p class="card-text"><?php echo html_escape($product["description"]); ?></p>
+									
+									 <a href="<?php echo ws_full_url($product);?>" class="card-link">Read more</a>
+    								<?php echo ws_archive_url($product, array("class"=>"card-link"));?>
+									<?php //print_r($product); ?>	
+								</div>
+								<div class="card-body">
+									<ul class="list-inline">
+										<li class="list-inline-item">Orders:<?php echo html_escape($product["orders"]); ?> </li>
+										<li class="list-inline-item">
+											Updated: <?php echo timespan(strtotime($product["modified_date"]))?> ago
+										</li>
+									</ul>
+								</div>
+								
+							</div>
+						</div>
+					<?php endforeach; ?>
+				<?php endif; ?>
+				
+			</div><!--end of .row -->
 
 			
 			
