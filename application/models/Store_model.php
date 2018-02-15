31f71d7a0931d3e97467990ec604c97a09c74f48
AUGUSTINE-PC\AUGUSTINE <rmoayinla@yahoo.com>
2018-02-15 16:22:39 +0100

    A lot of revamp and addition here

--- application/models/Store_model.php
+++ application/models/Store_model.php
@@ -16,8 +16,44 @@ class Store_model extends CI_Model{
 
 	private $tables  = null;
 
+	private $database = "";
+
+	private $result_type = "";
+
 	public function __construct(){
-		
+		$this->set_defaults();
+	}
+
+	private function results($query, $type = null){
+		$type = empty($type) ? $this->result_type : $type;
+		switch ($type){
+			case 'json':
+				$result = json_encode($query->result());
+				break;
+			case 'obj':
+			case 'object':
+				$result = $query->result();
+				break; 
+			case 'array':
+			default:
+				$result = $query->result_array();
+		}
+		return $result;
+	}
+	
+	/**
+	 * Set default values for our properties 
+	 *
+	 * default database, result type, table are set 
+	 */
+	private function set_defaults(){
+		$this->database = 'webstore';
+		$this->result_type = 'array';
+	}
+
+	public function get_defaults($prop){
+		if(property_exists($this, $prop))
+			return $this->{$prop};
 	}
 
 	/**
@@ -26,17 +62,19 @@ class Store_model extends CI_Model{
 	 * All database operations will only be performed on the active table 
 	 */
 	public function set_table($table){
+		//fetch tables from the db if they have not been fetched before//
 		if(empty($this->tables)){
-			throw new Exception('table has not been fetched yet');
+			$this->tables = $this->get_tables();
 		}
-		//prefix the passed table //
-		$table = strtolower('ws_'.$table);
+		
+		$table = strtolower($table);
 
+		//throw exception if the table does not exist in the database
 		if(!in_array($table, $this->tables)){
 			throw new Exception('table does not exist in database'); 
 		}
 
-		$this->table = $table;
+		$this->table = str_replace(' ', '_', $table);
 
 	}
 
@@ -55,7 +93,7 @@ class Store_model extends CI_Model{
 		$sql = $table_title = "";
 
 		// mysql statement for showing all tables //
-		$sql = 'SHOW tables from webstore';
+		$sql = "SHOW tables from $this->database";
 
 		$tables_query = $this->db->query($sql);
 		$tables = array();
@@ -65,16 +103,31 @@ class Store_model extends CI_Model{
 			return $tables;
 		}
 
-		$tables = array_column($tables_query->result_array(), 'Tables_in_webstore');
-		$this->tables = $tables;
+		$tables = array_column($tables_query->result_array(), sprintf('Tables_in_%s', $this->database));
+
 		$tables = array_map(function($name){
 			return str_ireplace(array('ws_', '_'), array('', ' '), $name);
-		}, $this->tables);
+		}, $tables);
+
+		$this->tables = $tables;
 		
-		return $tables;
+		return $this->tables;
 	}
 
-	public function get(){
+	/**
+	 * Load all data in a particular table 
+	 *@param: $table 			string 		table name (optional)
+	 		  $columns 			array 		select columns (optional)
+	 		  $result_type 		string 		the type of data to be returned 
+	 */
+	public function getAllFromTable($table = "", $columns = null, $result_type = ""){
+		if(empty($table)) $table = $this->get_table();
+		if(!empty($columns)){
+			$columns = implode(', ', $columns);
+			$this->db->select($columns);
+		}
+		$products = $this->db->get($table);
 		
+ 		return $this->results($products, $result_type);
 	}
 }
