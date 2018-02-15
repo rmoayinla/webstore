d54f701beb16634899852541d18ae7a77e00dc5b
AUGUSTINE-PC\AUGUSTINE <rmoayinla@yahoo.com>
2018-02-15 16:29:27 +0100

    now using moment and wyswig

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,20 @@
+ //moment.js for date manipulation and settings //
+ var moment = moment(); 
  (function($){
  	
+ 	if(typeof moment !== "undefined"){
+ 		console.log(moment.format());
+
+ 	}
 
  })(jQuery);
- 
+
  tinymce.init({
     selector: '.tinymce-editor',
-    menubar: 'edit view format',
+    menubar: false,
+    content_css: '',
+    browser_spellcheck: true,
+    mobile: {
+    	theme: 'mobile',
+    },
  });
