e3c68354545d269626a4732330873af2fda60375
AUGUSTINE-PC\AUGUSTINE <rmoayinla@yahoo.com>
2018-02-24 16:31:03 +0100

    the form is working but still doing some tweak, product_types are passed as hidden fields, maybe i will have to change that later

--- public/partials/new_book_form.php
+++ public/partials/new_book_form.php
@@ -1,23 +1,51 @@
-<?php echo form_open(current_url(), array("id" => "create-book")); ?>
+<?php if(!empty(validation_errors())): ?>
+	<div class="alert alert-warning"><?php echo validation_errors(); ?></div>
+<?php endif; ?>
+
+<?php echo form_open_multipart(current_url(), array("id" => "create-book", "class" => "form")); ?>
 	<div class="form-group">
 	    <label for="">Title </label>
-	    <?php echo form_input(array(
-	    	"name" => "title", "value" => set_value("title"), 
-	    	"placeholder" => sprintf('Enter title for %s', html_escape($product_type)),
-	    	"class" => "form-control"
-	    	)); 
+	    <?php echo form_input(
+	    		array(
+	    			"name" => "title", 
+	    			"value" => set_value("title"), 
+	    			"placeholder" => sprintf('Enter title for %s', html_escape($product_type["name"])),
+	    			"class" => "form-control"
+	    		)
+	    	); 
 	    ?>
-	    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+	    <small id="emailHelp" class="form-text text-muted"></small>
 	 </div>
 	 <div class="form-group">
 	    <label for="">Description</label>
-	    <?php echo form_textarea(array(
-	    	"name" => "description", "value" => set_value("description"), 
-	    	"placeholder" => sprintf('Enter description for %s', html_escape($product_type)),
-	    	"class" => "form-control tinymce-editor"
-	    	)); 
+	    <?php echo form_textarea(
+	    		array(
+	    			"name" => "description", 
+	    			"value" => set_value("description"), 
+	    			"placeholder" => sprintf('Enter description for %s', html_escape($product_type["name"])),
+	    			"class" => "form-control tinymce-editor"
+	    		)
+	    	); 
 	    ?>
-	    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+	    <small id="emailHelp" class="form-text text-muted"></small>
+	 </div>
+	 <div class="form-group">
+	 	<label for="featured-image" class="d-block p-4 text-center border"> Upload Featured Image </label>
+	 	<?php
+	 		echo form_upload(
+	 			array(
+	 				"name" => "thumbnail",
+	 				"class" => "form-control-file d-none",
+	 				"id" => "featured-image"
+	 			)
+	 		);
+		 ?>
+	 </div>
+	 
+	 <input type="hidden" name="product_type_id" value="<?php echo html_escape($product_type["ID"]); ?>" />
+	 <input type="hidden" name="product_type_name" value="<?php echo html_escape($product_type["name"]); ?>" />
+	 <div class="form-group">
+	 	<input type="submit" name="submit" value="Create book" class="btn btn-submit"/>
 	 </div>
 
-<?php echo form_close(); ?>
+<?php echo form_close(); print_r($product_type);?>
