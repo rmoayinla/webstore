2891728d32ad70942bf78fb1183cf434d9054087
AUGUSTINE-PC\AUGUSTINE <rmoayinla@yahoo.com>
2018-02-24 16:45:06 +0100

    Traits for Store model

---
+++ application/third_party/traits/Db_query.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * Database Query trait
+ *
+ * This class loads all the database methods for our store model
+ * the copy of CI Db is used here to query the db 
+ *
+ *@author: Rabiu Mustapha
+ *@category: Database model
+ *@version: 0.1
+ */
+
+trait DB_query{
+	
+	/**
+	 * Returns an instance of db object
+	 *
+	 * the db object is CI Database handler which does the actual querying 
+	 */
+	public function get_db(){
+		return $this->db;
+	}
+
+	public function query($sql){
+		$this->results = $this->db->query($sql);
+	}
+
+	/**
+	 * generate query results in different formats 
+	 *
+	 *@param: $type 	(string)	format can be array, obj or json
+	 */
+	public function get_results($type = null){
+		$type = empty($type) ? $this->get_defaults('result_type') : $type;
+		switch ($type){
+			case 'json':
+				$result = is_object($this->results) ? json_encode($this->results->result()) : json_encode($this->results);
+				break;
+			case 'obj':
+			case 'object':
+				$result = method_exists($this->result, 'result') ? $this->results->result() : $this->results;
+				break; 
+			case 'array':
+			default:
+				$result = method_exists($this->results, 'result_array') ? $this->results->result_array() : (array)$this->results;
+		}
+		return $result;
+	}
+
+	/**
+	 * Load all data in a particular table 
+	 *@param: $columns 			array|string		select columns (optional)
+	 *@return: $this			object 				instance of this class 
+	 *
+	 */
+	public function getAllFromTable($columns = array()){
+		
+		//get the active table //
+		$table = $this->get_table();
+
+		//select only particular columns from the table if columns are passed //
+		if(!empty($columns)){
+			$columns = is_array($columns) ? implode(', ', $columns) : (string) $columns;
+			$this->db->select($columns);
+		}
+		//query the database, and store the raw result //
+		$this->results = $this->db->get($table);
+		
+ 		return $this;
+	}
+
+	/**
+	 * Get only one data from the database 
+	 *@param: $column 		array 		array of columns to select from database
+	 *@return: $this 		object 		instance of Store_model class
+	 *
+	 */
+	public function getOneFromTable($columns=array()){
+		//get the active table //
+		$table = $this->get_table();
+
+		//select only particular columns from the table if columns are passed //
+		if(!empty($columns)){
+			$columns = is_array($columns) ? implode(', ', $columns) : (string) $columns;
+			$this->db->select($columns);
+		}
+		//limit this query to only one result //
+		$this->db->limit(1);
+
+		//query the database, and store the raw result //
+		$this->results = $this->db->get($table);
+		
+ 		return $this;
+	}
+
+	
+	/**
+	 * Populate related datas from relationship tables 
+	 *
+	 * queries the database and get datas of related columns 
+	 * one to many or many to one relationships are loaded here 
+	 * @param: 	$column 		string|array 		columns with foreign keys 
+	 *@return: 	$this 			object 				instance of this class
+	 *
+	 */
+	public function populate($column){
+		//check if the stored result is an instance of CI_DB result, if so convert to array //
+		if(method_exists($this->results, 'result') || method_exists($this->results, 'result_array'))
+			$this->results = $this->get_results();
+		
+		//copy or clone our results here, since it will be overriden in our next query //
+		$clone_results  = $this->results;
+
+		/**
+		 * Loop through our current results and check each result if the column is a foreign key
+		 * checks the foreign key column in our relationships, this will tell us which table to map to 
+		 * query the table and get the populated data, add the populated data to our cloned result 
+		 */
+		foreach($this->results as $index => $result){
+			
+			if(!empty($column)){
+				if(!array_key_exists($column, $result)) return $this;
+				if(!array_key_exists($column, $this->relationships)) return $this;
+				
+				try{
+					$table = str_replace('_', ' ', $this->relationships[$column]);
+					$sub_table = $this->set_table($table);
+					$result[$column] = $this->getAllFromTable()->get_results();
+					$clone_results[$index] = $result;
+				} catch(Exception $e){
+					throw new Exception($e->getMessage());
+				}
+			}
+		}
+		//copy our result back to the stored results //
+		$this->results =  $clone_results;
+		return $this;
+	}
+}
---
+++ application/third_party/traits/Db_table.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Database table trait for store model 
+ *
+ * This class handles all the database table settings for store model
+ * setting of the active table, swapping tables and getting all database tables 
+ *
+ */
+
+trait Db_Table{
+
+	/**
+	 * Set the active table 
+	 *
+	 * All database operations will only be performed on the active table 
+	 */
+	public function set_table($table){
+		//fetch tables from the db if they have not been fetched before//
+		if(empty($this->tables)){
+			$this->tables = $this->get_tables();
+		}
+		
+		$table = strtolower($table);
+
+		//throw exception if the table does not exist in the database
+		if(!in_array($table, $this->tables)){
+			throw new Exception('table does not exist in database'); 
+		}
+
+		$this->table = str_replace(' ', '_', $table);
+
+		return $this;
+
+	}
+
+	/**
+	 * Returns the active table 
+	 */
+	public function get_table(){
+		return !empty($this->table) ? $this->table  : $this->get_defaults('table');
+	}
+
+	/**
+	 * Return all tables from the webstore database 
+	 *
+	 */
+	public function get_tables(){
+		$sql = $table_title = "";
+
+		// mysql statement for showing all tables //
+		$sql = "SHOW tables from $this->database";
+
+		$tables_query = $this->db->query($sql);
+		$tables = array();
+		
+		//bail early if we have empty tables//
+		if(empty($tables_query->result_array())){
+			return $tables;
+		}
+
+		$tables = array_column($tables_query->result_array(), sprintf('Tables_in_%s', $this->database));
+
+		$tables = array_map(function($name){
+			return str_ireplace(array('ws_', '_'), array('', ' '), $name);
+		}, $tables);
+
+		$this->tables = $tables;
+		
+		return $this->tables;
+	}
+}
