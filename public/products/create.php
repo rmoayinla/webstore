44f1f64d86903ee60a4530796bd0075d4cf79421
AUGUSTINE-PC\AUGUSTINE <rmoayinla@yahoo.com>
2018-02-24 16:31:20 +0100

    the template file for displaying the form

---
+++ public/products/create.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+	<!--include and import header partial -->
+	<?php include VIEWPATH.'partials/header.php'; ?>
+
+		<header class="jumbotron jumbotron-fluid">
+			<div class="container-fluid">
+				<h1>Create new <?php //if(!empty($product_type)) echo html_escape($product_type); ?> </h1>
+			</div>
+		</header>
+		<main id="content" class='container-fluid'>
+			<div class="row">
+				<div class="form-wrapper col-md-6 mx-auto">
+					<!--include the form for new book partial-->
+					<?php include VIEWPATH.'partials/new_book_form.php'; ?>
+				</div>
+			</div>
+				
+			
+
+			
+		</main>
+
+	<!-- include and import the site footer partial -->
+	<?php include VIEWPATH.'partials/footer.php'; ?>
