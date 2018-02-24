e17c6190688d2f21100f42a083004e28b989d492
AUGUSTINE-PC\AUGUSTINE <rmoayinla@yahoo.com>
2018-02-24 16:35:56 +0100

    Now extending store_model instead of CI_model, easier to perform actions now

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -1,9 +1,25 @@
 <?php
- class Product_model extends CI_Model{
+/**
+ * A simple model for handling the products table 
+ *
+ * CRUD (create, read, update and delete operations are done on the database)
+ *
+ */
+require_once APPPATH."models/store_model.php";
+
+class Product_model extends Store_Model{
+
+ 	
 
  	public function __construct(){
+ 		parent::__construct();
+		$this->set_table('products');
+		$this->set_defaults('relationships', ['product_type_id' => 'products_type']);
  		
+ 		$config = [];
+ 		$this->load->library('upload', $config);
  	}
+
  	public function getAll(){
  		$products = $this->db->get('products');
  		return $products->result_array();
