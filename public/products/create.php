80ff0576fc3fcd7732b8aa96323b61588f9dafa0
AUGUSTINE-PC\AUGUSTINE <rmoayinla@yahoo.com>
2018-03-03 18:45:11 +0100

    Now showing two different partials, one partial for when the product has been inserted successfuly and the order in case of errors

--- public/products/create.php
+++ public/products/create.php
@@ -6,14 +6,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 		<header class="jumbotron jumbotron-fluid">
 			<div class="container-fluid">
-				<h1>Create new <?php //if(!empty($product_type)) echo html_escape($product_type); ?> </h1>
+				<h1>Create new <?php if(!empty($product_type)) echo html_escape($product_type["name"]); ?> </h1>
 			</div>
 		</header>
 		<main id="content" class='container-fluid'>
 			<div class="row">
 				<div class="form-wrapper col-md-6 mx-auto">
-					<!--include the form for new book partial-->
-					<?php include VIEWPATH.'partials/new_book_form.php'; ?>
+					<?php if(isset($created) && $created === TRUE) : ?>
+						<!--include the form for new book partial-->
+						<?php include VIEWPATH.'partials/new_book_success.php'; ?>
+					<?php else: ?>
+						<!--include the form for new book partial-->
+						<?php include VIEWPATH.'partials/new_book_form.php'; ?>
+					<?php endif; ?>
 				</div>
 			</div>
 				
